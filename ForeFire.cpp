66e4499eeb5c4eab68e0be42cf0f8aec1c3b92df
xinshoujiajia <62921971+xinshoujiajia@users.noreply.github.com>
2021-01-07 17:43:06 +0800

    Update ForeFire.cpp

--- ForeFire.cpp
+++ ForeFire.cpp
@@ -6,5 +6,20 @@ void Tower::Fire(Enemy* e)
   float b=2.0f*e->GetMoveComponent()->GetVel();
     *Math::dot(dis,e->GetToward);
   float c=dis.LengthSq();
-    
+  float t;
+  float disc=b*b-4.0f*a*c;
+  if(disc>0.0f)
+  {
+    disc=Math::Sqrt(disc);
+    t=(-b-disc)/(2.0f*a);
+    if(t>0.0f)
+    {
+      Vector2 toward=dis+e->GetToward()*t*e->GetMoveComponent()->GetVel();
+      toward.Normlize();
+      Bullet* bullet=new Bullet(GetGame());
+      bullet->SetToward(toward);
+      bullet->SetVel(vel);
+      bullet->SetLife(t);
+    }
+  }
 }
